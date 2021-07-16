7c1ec7d04c2383d88ee89bf694c48d46698650bf
Link <xieaolin@gmail.com>
2021-07-16 14:42:31 +0800

    fix: update config structure for deployment

--- config/quote.ts
+++ config/quote.ts
@@ -1,13 +1,21 @@
 import { INFO_DATA_TYPE } from '../src/utils/const'
 
+const PayersLockScript = {
+  codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+  hashType: 'type',
+  args: '0xb12e3692d401c331f6d1f1efcb24d510296c4a6a',
+}
+
+const PayersPrivateKey = '0x986ac023216102573529e19d79368cbbe84a5c5987ea4f2b8169ffaea50dcf0e'
+
 const IndexStateTypeScript = {
-  codeHash: '0x6a9a78dca5d74ff96ffad1e0bc52e22afe68e3b088334b25bcbedef725f76cd2',
+  codeHash: '0x554cff969f3148e3c620749384004e9692e67c429f621554d139b505a281c7b8',
   hashType: 'type',
   args: '0x00',
 }
 
 const InfoTypeScript = {
-  codeHash: '0x8ebe174552f3cddded1fcb4e427562e5a535f3d32f1d5f0ea0cf4578ffaf63ca',
+  codeHash: '0x96248cdefb09eed910018a847cfb51ad044c2d7db650112931760e3ef34a7e9a',
   hashType: 'type',
   args: '0x00',
 }
@@ -15,9 +23,9 @@ const InfoTypeScript = {
 console.log('using config: quote')
 
 export default {
-  BLOCKS_INTERVAL: 5,
-
   infoDataType: INFO_DATA_TYPE.arbitrage,
   IndexStateTypeScript,
   InfoTypeScript,
+  PayersLockScript,
+  PayersPrivateKey,
 }
