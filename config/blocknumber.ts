690ce83122b236a94bddbc66afa144dc8b0f84b5
Link <xieaolin@gmail.com>
2021-07-16 14:42:31 +0800

    fix: update config structure for deployment

--- config/blocknumber.ts
+++ config/blocknumber.ts
@@ -1,13 +1,21 @@
 import { INFO_DATA_TYPE } from '../src/utils/const'
 
+const PayersLockScript = {
+  codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+  hashType: 'type',
+  args: '0xa897829e60ee4e3fb0e4abe65549ec4a5ddafad7',
+}
+
+const PayersPrivateKey = '0x5d7332d197151cee989f106a2549a2b68c789953f1d768e56bd1516df231a053'
+
 const IndexStateTypeScript = {
-  codeHash: '0x6a9a78dca5d74ff96ffad1e0bc52e22afe68e3b088334b25bcbedef725f76cd2',
+  codeHash: '0x554cff969f3148e3c620749384004e9692e67c429f621554d139b505a281c7b8',
   hashType: 'type',
   args: '0x02',
 }
 
 const InfoTypeScript = {
-  codeHash: '0x8ebe174552f3cddded1fcb4e427562e5a535f3d32f1d5f0ea0cf4578ffaf63ca',
+  codeHash: '0x96248cdefb09eed910018a847cfb51ad044c2d7db650112931760e3ef34a7e9a',
   hashType: 'type',
   args: '0x02',
 }
@@ -15,9 +23,9 @@ const InfoTypeScript = {
 console.log('using config: blocknumber')
 
 export default {
-  BLOCKS_INTERVAL: 2,
-
   infoDataType: INFO_DATA_TYPE.blocknumber,
   IndexStateTypeScript,
   InfoTypeScript,
+  PayersLockScript,
+  PayersPrivateKey,
 }
--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,9 @@
 import { INFO_DATA_TYPE } from '../src/utils/const'
 
 const PayersLockScript = {
-  codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+  codeHash: '',
   hashType: 'type',
-  args: '0xec17aa5219aa210e73605c06c5414860e0c5e8e4',
+  args: '',
 }
 
 const PayersPrivateKey = ''
@@ -11,24 +11,24 @@ const PayersPrivateKey = ''
 const CellDeps = [
   // index-state-cell-type & info-cell-type, this need to be updated every time the contracts changed
   {
-    outPoint: { txHash: '0x79874d992c6b353338e21d0c2a2e88083b049ad08b7a3e46d36eef2a98ee08bd', index: '0x0' },
+    outPoint: { txHash: '0x81cc4a241bd376071de687a60c1dc27761c2d2604999447bad82451ec80e6ec4', index: '0x0' },
     depType: 'depGroup',
   },
   // ckb signall lock
   {
-    outPoint: { txHash: '0x419014a3e2067ef4b4cf3cd75d0c4382df4bf9718cbed63d308edb01654846c9', index: '0x0' },
+    outPoint: { txHash: '0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37', index: '0x0' },
     depType: 'depGroup',
   },
 ]
 
 const IndexStateTypeScript = {
-  codeHash: '0x6a9a78dca5d74ff96ffad1e0bc52e22afe68e3b088334b25bcbedef725f76cd2',
+  codeHash: '',
   hashType: 'type',
   args: '0x',
 }
 
 const InfoTypeScript = {
-  codeHash: '0x8ebe174552f3cddded1fcb4e427562e5a535f3d32f1d5f0ea0cf4578ffaf63ca',
+  codeHash: '',
   hashType: 'type',
   args: '0x',
 }
@@ -40,8 +40,6 @@ export default {
   CKB_NODE_RPC: 'http://127.0.0.1:8114',
   CKB_NODE_INDEXER: 'http://127.0.0.1:8116',
   CKB_WS_URL: 'ws://127.0.0.1:8118',
-
-  BLOCKS_INTERVAL: 3,
 
   infoDataType: INFO_DATA_TYPE.arbitrage,
 
