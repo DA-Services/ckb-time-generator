47cd1d7c54aa4a30ebbdc90583591c799f506db0
Link <link@blockabc.com>
2021-08-16 10:20:33 +0800

    chore: add maximum memory limit to processes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,7 @@ function generateApp (name, entry, args, env) {
     log_date_format: 'MM-DD HH:mm:ss',
     kill_timeout: 10 * 1000, // ms
     listen_timeout: 10 * 1000, // ms
+    max_memory_restart: '256M',
     env: {
       NODE_ENV: env,
       NODE_CONFIG_DIR: './dist/config',
