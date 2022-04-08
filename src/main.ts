aaa2823b0305498727fb89eb839a305fff593125
Link <link@blockabc.com>
2022-04-08 15:42:31 +0800

    fix: temporarily suppress a type error

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,13 @@ yargs(process.argv.slice(2))
       .option('type', { alias: 't', desc: 'Type of cell', required: true, choices: ['timestamp', 'blocknumber', 'quote']})
       .example('$0 --type timestamp', 'Check status of TimeCells.')
   }, statusController)
-  .command('fix', 'Recycle redundant IndexStateCells and InfoCells on blockchain.', (yargs) => {
-    return yargs
-      .option('type', { alias: 't', desc: 'Type of cell', required: true, choices: ['timestamp', 'blocknumber', 'quote']})
-      .option('dry-run', { alias: 'd', desc: 'Run fix command once to see if the result is expected.', default: false, boolean: true })
-      .example('$0 --type timestamp', 'Recycle redundant TimeCells and their IndexStateCells.')
-  }, fixController)
+  // TODO Fix this command, it is a index signature error cause the codes can not pass compiling.
+  // .command('fix', 'Recycle redundant IndexStateCells and InfoCells on blockchain.', (yargs) => {
+  //   return yargs
+  //     .option('type', { alias: 't', desc: 'Type of cell', required: true, choices: ['timestamp', 'blocknumber', 'quote']})
+  //     .option('dry-run', { alias: 'd', desc: 'Run fix command once to see if the result is expected.', default: false, boolean: true })
+  //     .example('$0 --type timestamp', 'Recycle redundant TimeCells and their IndexStateCells.')
+  // }, fixController)
   .command('create', 'Create required IndexStateCells and InfoCells on requirement.', (yargs) => {
     return yargs
       .option('type', { alias: 't', desc: 'Type of cell', required: true, choices: ['timestamp', 'blocknumber', 'quote']})
